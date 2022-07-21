85976a7dad502386224d276c0531ba3a2f010c36
highlander1337 <tassioleno@gmail.com>
2022-07-21 17:02:54 -0300

    nothing to say

---
+++ Courses/Coursera/Introduction-to-electronics/week-2/README.md
@@ -0,0 +1,1 @@
+# Introduction to Op Amps and Ideal Behavior
---
+++ Courses/Coursera/Software-engineering-specialization/Software-engineering-modeling-software-systems-using-UML/week-2/projects/0-understanding-classes-in-matlab/motor.m
@@ -0,0 +1,23 @@
+classdef motor
+    %Motor Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        impedance
+    end
+    
+    methods
+        function obj = untitled4(inputArg1,inputArg2)
+            %UNTITLED4 Construct an instance of this class
+            %   Detailed explanation goes here
+            obj.Property1 = inputArg1 + inputArg2;
+        end
+        
+        function outputArg = method1(obj,inputArg)
+            %METHOD1 Summary of this method goes here
+            %   Detailed explanation goes here
+            outputArg = obj.Property1 + inputArg;
+        end
+    end
+end
+
